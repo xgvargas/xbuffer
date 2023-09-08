998a492c541f69ae3a96d2249002ea2c5348e2fa
Gustavo Vargas <xgvargas@gmail.com>
2023-09-08 13:44:13 -0300

    renamed type and added comments

--- buffer.go
+++ buffer.go
@@ -4,8 +4,12 @@ https://medium.com/learning-the-go-programming-language/encoding-data-with-the-g
 
 */
 
+// Package xbuffer is a package to write and read data with a flexible format both in its size and endianess.
 package xbuffer
 
+import "fmt"
+
+// Order define if buffer content is ordered as Little or Big endian
 type Order int
 
 const (
@@ -13,60 +17,71 @@ const (
 	BigEndian
 )
 
-type Buffer struct {
+// Xbuffer holds the internal buffer
+type Xbuffer struct {
 	buf []byte
 	dir Order
 }
 
-// creates a new Buffer starting `size` bytes long
-func NewBuffer(size int, dir Order) Buffer {
+// NewBuffer creates a new Xbuffer starting `size` bytes long
+func NewBuffer(size int, dir Order) Xbuffer {
 
-	return Buffer{
+	return Xbuffer{
 		make([]byte, size),
 		dir,
 	}
 }
 
-// create a new Buffer and sets its content to a copy of `data`
-func NewBufferFrom(data []byte, dir Order) Buffer {
+// NewBufferFrom create a new Xbuffer and sets its content to a copy of `data`
+func NewBufferFrom(data []byte, dir Order) Xbuffer {
 
 	b := make([]byte, len(data))
 
 	copy(b, data)
 
-	return Buffer{
+	return Xbuffer{
 		b,
 		dir,
 	}
 }
 
-func (b *Buffer) fixLen(size int) {
-	// if size > len(b.buf) {
-	// 	// extra := make([]byte, size-len(b.buf))
-	// 	// b.buf = append(b.buf, extra)
-	// }
+func (b *Xbuffer) fixLen(last int) {
+
+	if last >= len(b.buf) {
+
+		panic(fmt.Sprintf("Trying to write to address %d of %d bytes long xbuffer", last, len(b.buf)))
+
+		// TODO optionally grow internal buffer if necessary
+		// extra := make([]byte, last-len(b.buf))
+		// b.buf = append(b.buf, extra)
+	}
 }
 
-func (b *Buffer) GetSlice() []byte {
+// GetSlice returns the internal buffer
+func (b *Xbuffer) GetSlice() []byte {
 	return b.buf
 }
 
-func (b *Buffer) ReadSlice(offset, size int) []byte {
+// ReadSlice returns a slice of internal buffer
+func (b *Xbuffer) ReadSlice(offset, size int) []byte {
 	tmp := make([]byte, size)
 	copy(tmp, b.buf[offset:offset+size])
 	return tmp
 }
 
-func (b *Buffer) ReadU8(offset int) uint8 {
+// ReadU8 returns unsigned 8 bits from offset in internal buffer
+func (b *Xbuffer) ReadU8(offset int) uint8 {
 	return b.buf[offset]
 }
 
-func (b *Buffer) ReadI8(offset int) int8 {
+// ReadI8 returns signed 8 bits from offset in internal buffer
+func (b *Xbuffer) ReadI8(offset int) int8 {
 	return int8(b.buf[offset])
 }
 
-func (b *Buffer) ReadU16(offset int) uint16 {
-	var d uint16 = 0
+// ReadU16 returns unsigned 16 bits from offset in internal buffer
+func (b *Xbuffer) ReadU16(offset int) uint16 {
+	var d uint16
 	if b.dir == LittleEndian {
 		d |= uint16(b.buf[offset+0])
 		d |= uint16(b.buf[offset+1]) << 8
@@ -77,12 +92,14 @@ func (b *Buffer) ReadU16(offset int) uint16 {
 	return d
 }
 
-func (b *Buffer) ReadI16(offset int) int16 {
+// ReadI16 returns signed 16bits from offset in internal buffer
+func (b *Xbuffer) ReadI16(offset int) int16 {
 	return int16(b.ReadU16(offset))
 }
 
-func (b *Buffer) ReadU32(offset int) uint32 {
-	var d uint32 = 0
+// ReadU32 returns unsigned 32 bits from offset in internal buffer
+func (b *Xbuffer) ReadU32(offset int) uint32 {
+	var d uint32
 	if b.dir == LittleEndian {
 		d |= uint32(b.buf[offset+0])
 		d |= uint32(b.buf[offset+1]) << 8
@@ -97,12 +114,14 @@ func (b *Buffer) ReadU32(offset int) uint32 {
 	return d
 }
 
-func (b *Buffer) ReadI32(offset int) int32 {
+// ReadI32 returns signed 32 bits from offset in internal buffer
+func (b *Xbuffer) ReadI32(offset int) int32 {
 	return int32(b.ReadU32(offset))
 }
 
-func (b *Buffer) ReadU64(offset int) uint64 {
-	var d uint64 = 0
+// ReadU64 returns unsigned 64 bits from offset in internal buffer
+func (b *Xbuffer) ReadU64(offset int) uint64 {
+	var d uint64
 	if b.dir == LittleEndian {
 		d |= uint64(b.buf[offset+0])
 		d |= uint64(b.buf[offset+1]) << 8
@@ -125,20 +144,24 @@ func (b *Buffer) ReadU64(offset int) uint64 {
 	return d
 }
 
-func (b *Buffer) ReadI64(offset int) int64 {
+// ReadI64 returns signed 64 bits from offset in internal buffer
+func (b *Xbuffer) ReadI64(offset int) int64 {
 	return int64(b.ReadU64(offset))
 }
 
-func (b *Buffer) WriteU8(offset int, val uint8) {
+// WriteU8 write unsigned 8 bits value to internal buffer at offset
+func (b *Xbuffer) WriteU8(offset int, val uint8) {
 	b.fixLen(offset + 1)
 	b.buf[offset] = val
 }
 
-func (b *Buffer) WriteI8(offset int, val int8) {
+// WriteI8 write signed 8 bits value to internal buffer at offset
+func (b *Xbuffer) WriteI8(offset int, val int8) {
 	b.buf[offset] = byte(val)
 }
 
-func (b *Buffer) WriteU16(offset int, val uint16) {
+// WriteU16 write unsigned 16 bits value to internal buffer at offset
+func (b *Xbuffer) WriteU16(offset int, val uint16) {
 	b.fixLen(offset + 2)
 	if b.dir == LittleEndian {
 		b.buf[offset] = byte(val & 0xff)
@@ -149,11 +172,13 @@ func (b *Buffer) WriteU16(offset int, val uint16) {
 	}
 }
 
-func (b *Buffer) WriteI16(offset int, val int16) {
+// WriteI16 write signed 16 bits value to internal buffer at offset
+func (b *Xbuffer) WriteI16(offset int, val int16) {
 	b.WriteU16(offset, uint16(val))
 }
 
-func (b *Buffer) WriteU32(offset int, val uint32) {
+// WriteU32 write unsigned 32 bits value to internal buffer at offset
+func (b *Xbuffer) WriteU32(offset int, val uint32) {
 	b.fixLen(offset + 4)
 	if b.dir == LittleEndian {
 		b.buf[offset] = byte(val & 0xff)
@@ -169,11 +194,13 @@ func (b *Buffer) WriteU32(offset int, val uint32) {
 
 }
 
-func (b *Buffer) WriteI32(offset int, val int32) {
+// WriteI32 write signed 32 bits value to internal buffer at offset
+func (b *Xbuffer) WriteI32(offset int, val int32) {
 	b.WriteU32(offset, uint32(val))
 }
 
-func (b *Buffer) WriteU64(offset int, val uint64) {
+// WriteU64 write unsigned 64 bits value to internal buffer at offset
+func (b *Xbuffer) WriteU64(offset int, val uint64) {
 	b.fixLen(offset + 8)
 	if b.dir == LittleEndian {
 		b.buf[offset+0] = byte(val & 0xff)
@@ -197,10 +224,13 @@ func (b *Buffer) WriteU64(offset int, val uint64) {
 
 }
 
-func (b *Buffer) WriteI64(offset int, val int64) {
+// WriteI64 write signed 64 bits value to internal buffer at offset
+func (b *Xbuffer) WriteI64(offset int, val int64) {
 	b.WriteU64(offset, uint64(val))
 }
 
-func (b *Buffer) WriteSlice(offset int, from []byte) {
+// WriteSlice write a slice to internal buffer at offset
+func (b *Xbuffer) WriteSlice(offset int, from []byte) {
+	b.fixLen(offset + len(from))
 	copy(b.buf[offset:], from)
 }
--- doc.go
+++
@@ -1,4 +0,0 @@
-/*
-xbuffer is a package to write and read data with a flexible format both in its size and endianess.
-*/
-package xbuffer
